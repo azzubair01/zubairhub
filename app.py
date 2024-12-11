c00db2ba4079e696bed60e568146ad976f490e21
Azzubair <azzubairazeman@gmail.com>
2024-12-11 18:32:44 +0800

    added object detection module

--- app.py
+++ app.py
@@ -5,18 +5,30 @@ import requests
 from modules.social_graph import (family_graph)
 from modules.introduction import (intro)
 from modules.personal import transform_sap_data
-# from modules.computer_vision import (detect_object)
+from modules.computer_vision import detect_object
 
 
 st.set_page_config(page_title="Azzubair's Webapp", page_icon="📊", layout='centered')
 
 st.sidebar.title('My Profile')
-image = Image.open(BytesIO(requests.get(
-    "https://avatars.githubusercontent.com/u/52235533?s=400&u=933b82feeb4ec3f9278f6b66cf02d9d5ad351a72&v=4").content))
-newsize = (300, 300)
-image = image.resize(newsize)
 
-st.sidebar.image(image)
+style_image1 = """
+width: auto;
+max-width: 150px;
+height: auto;
+max-height: 500px;
+display: block;
+margin-left: auto;
+margin-right: auto;
+justify-content: center;
+border-radius: 25%;
+"""
+
+st.sidebar.markdown(
+    f'<img src="{"https://avatars.githubusercontent.com/u/52235533?s=400&u=933b82feeb4ec3f9278f6b66cf02d9d5ad351a72&v=4"}" style="{style_image1}">',
+    unsafe_allow_html=True,
+)
+
 text = '<p style="font-family:sans-serif; color:Black; font-size: 16px;">' \
        '◉ Muhammad Azzubair bin Azeman </br>' \
        '◉ Gombak, Selangor</br>' \
@@ -28,8 +40,9 @@ st.sidebar.title('Navigation')
 page_names_to_func = {
     'Introduction': intro,
     'Family Graph': family_graph,
+    'Object Detection': detect_object,
     'Personal': transform_sap_data,
-    # 'Computer Vision': detect_object
+
 }
 
 project_select = st.sidebar.radio('Select project to display:', (list(page_names_to_func.keys())))
--- modules/computer_vision.py
+++ modules/computer_vision.py
@@ -1,27 +1,63 @@
+import json
+import os
 
-def detect_labels(path):
-    """Detects labels in the file."""
-    from google.cloud import vision
+from PIL import Image
+import streamlit as st
+from io import BytesIO
+from modules.utils.object_detection import DetrObjectDetection
 
-    client = vision.ImageAnnotatorClient()
+detector = DetrObjectDetection()
 
-    with open(path, "rb") as image_file:
-        content = image_file.read()
+def detect_object():
+    option = st.radio(label='Select image option:', options=('Example', 'Upload'), key='Radio button for object detection option', horizontal=True)
+    if option == 'Upload':
+        # Streamlit file uploader for image input
+        uploaded_file = st.file_uploader("Choose a file", type=['jpg', 'jpeg', 'png'])
+    elif option=='Example':
+        example_image_list = os.listdir('modules/data')
+        selected_image = st.selectbox(label='Select Example image', options=example_image_list)
+        uploaded_file = f'modules/data/{selected_image}'
 
-    image = vision.Image(content=content)
+    # Throw error if no file is uploaded
+    if uploaded_file is None:
+        st.error("No file uploaded yet.")
+    else:
+        try:
+            # Load image from the uploaded file
+            img = Image.open(uploaded_file)
+            with st.expander(label='Preview image:', expanded=False):
+                st.image(img)
+        except Exception as e:
+            st.error(f"Error opening image: {str(e)}")
 
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-    print("Labels:")
+        # Run object detection
+        with st.spinner("Running object detection..."):
+            labeled_image, detections = detector.detect_objects(img)
 
-    for label in labels:
-        print(label.description)
+        # Provide download options if detections are successful
+        if labeled_image and detections:
 
-    if response.error.message:
-        raise Exception(
-            "{}\nFor more info on error messages, check: "
-            "https://cloud.google.com/apis/design/errors".format(response.error.message)
-        )
+            # Create image buffer for download
+            buf = BytesIO()
+            labeled_image.save(buf, format="PNG")  # Save the labeled image into the buffer
+            byte_im = buf.getvalue()  # Get byte data from buffer
 
+            # Display the results and allow image download
+            st.subheader("Object Detection Predictions")
+            st.image(labeled_image)  # Display annotated image
 
-detect_labels('modules/data/setagaya_small.jpeg')
+            # Add a download button for the annotated image
+            st.download_button(
+                label='Download Image',
+                data=byte_im,
+                file_name="image_object_detection.png",
+                mime="image/png",
+                type='primary',
+                use_container_width=True
+            )
+            # Display and allow download of predictions as JSON
+            # st.json(detections)  # Display the detections as a JSON object
+
+        else:
+            # Handle case where no objects are detected
+            st.warning("No objects detected.")
