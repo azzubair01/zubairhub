9e88b1b162956fa228d50b577e3e3e2568ecc2ea
Azzubair <azzubairazeman@gmail.com>
2026-06-14 13:21:45 +0800

    refactor: migrate family graph from Bokeh to Plotly for better interactivity

--- modules/social_graph.py
+++ modules/social_graph.py
@@ -1,15 +1,9 @@
 import networkx as nx
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid import AgGrid, GridUpdateMode
-from bokeh.plotting import Figure, from_networkx
-from bokeh.palettes import Spectral4
-from bokeh.layouts import gridplot
-from bokeh.models import ColumnDataSource, LabelSet, BoxSelectTool, Circle, HoverTool, MultiLine, NodesAndLinkedEdges, \
-    Range1d
-from bokeh.models.tools import TapTool, PanTool, WheelZoomTool, SaveTool, ResetTool
-
 
 @st.fragment
 def family_graph():
@@ -38,8 +32,8 @@ def family_graph():
 
     if update_button:
         index = len(family_df)
-        family_df = family_df.append(
-            pd.DataFrame({'Name': name, 'Parent': parent, 'Relationship': relationship}, index=[index]))
+        new_row = pd.DataFrame({'Name': name, 'Parent': parent, 'Relationship': relationship}, index=[index])
+        family_df = pd.concat([family_df, new_row])
         family_df.to_excel('modules/azzubair_family.xlsx', index=False)
 
     # Display in table
@@ -56,12 +50,11 @@ def family_graph():
         fit_columns_on_grid_load=True,
         theme="alpine")
 
-    sel_row_df = pd.DataFrame(grid_table["selected_rows"]).iloc[:, :1]
+    sel_row_df = pd.DataFrame(grid_table["selected_rows"])
 
     if not sel_row_df.empty:
-        sel_row_df = pd.DataFrame(grid_table["selected_rows"]).iloc[:, :1]
-        sel_row_df['index'] = sel_row_df['_selectedRowNodeInfo'].apply(lambda x: x['nodeRowIndex'])
-        sel_row_list = sel_row_df['index'].tolist()
+        # Assuming the structure of selected_rows based on previous code
+        sel_row_list = sel_row_df['_selectedRowNodeInfo'].apply(lambda x: x['nodeRowIndex']).tolist()
         col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
         with col4:
             button_delete = st.button('Delete')
@@ -73,49 +66,50 @@ def family_graph():
 
         G = nx.from_pandas_edgelist(family_df, source='Name', target='Parent', edge_attr=True)
 
-        # Generate the layout and set the 'pos' attribute
-        pos = nx.drawing.layout.spring_layout(G)
-        nx.set_node_attributes(G, pos, 'pos')
+        pos = nx.spring_layout(G)
 
         edge_x = []
         edge_y = []
         for edge in G.edges():
-            x0, y0 = G.nodes[edge[0]]['pos']
-            x1, y1 = G.nodes[edge[1]]['pos']
-            edge_x.append(x0)
-            edge_x.append(x1)
-            edge_x.append(None)
-            edge_y.append(y0)
-            edge_y.append(y1)
-            edge_y.append(None)
+            x0, y0 = pos[edge[0]]
+            x1, y1 = pos[edge[1]]
+            edge_x.extend([x0, x1, None])
+            edge_y.extend([y0, y1, None])
 
-        graph_renderer = from_networkx(G, layout_function=nx.spring_layout)
-        plot = Figure(height=550, width=600, tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
-                      x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1), title='')
-        plot.title.text = ""
-        graph_renderer.node_renderer.glyph = Circle(radius=0.01, fill_color=Spectral4[3])
-        graph_renderer.node_renderer.selection_glyph = Circle(radius=0.01, fill_color=Spectral4[2])
-        graph_renderer.node_renderer.hover_glyph = Circle(radius=0.01, fill_color=Spectral4[1])
-        graph_renderer.edge_renderer.glyph = MultiLine(line_color="lightblue", line_width="edge_width", )
-        graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color=Spectral4[2], line_width="edge_width")
-        graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], line_width="edge_width")
-        graph_renderer.selection_policy = NodesAndLinkedEdges()
-        graph_renderer.inspection_policy = NodesAndLinkedEdges()
-        HOVER_TOOLTIPS = [("Item", "@index")]
-        plot.add_tools(HoverTool(tooltips=HOVER_TOOLTIPS), WheelZoomTool(), TapTool(), BoxSelectTool(), PanTool(),
-                       SaveTool(), ResetTool())
-        plot.renderers.append(graph_renderer)
-        x, y = zip(*graph_renderer.layout_provider.graph_layout.values())
-        node_labels = list(graph_renderer.layout_provider.graph_layout.keys())
-        source = ColumnDataSource({'x': x, 'y': y,
-                                   'item': [node_labels[i] for i in range(len(x))]})
-        labels = LabelSet(x='x', y='y', text='item', source=source,
-                          background_fill_color='white', text_font_size='10px')
-        plot.renderers.append(labels)
-        # plot.outline_line_color = None
-        # plot.xgrid.grid_line_color = None
-        # plot.ygrid.grid_line_color = None
-        # plot.axis.visible = False
-        #
-        # # Plot network graph
-        st.bokeh_chart(gridplot([[plot]], sizing_mode='stretch_width'), use_container_width=True)
+        edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        node_x = []
+        node_y = []
+        node_text = []
+        for node in G.nodes():
+            x, y = pos[node]
+            node_x.append(x)
+            node_y.append(y)
+            node_text.append(node)
+
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers+text',
+            hoverinfo='text',
+            text=node_text,
+            textposition="top center",
+            marker=dict(
+                showscale=False,
+                color='lightblue',
+                size=10,
+                line_width=2))
+
+        fig = go.Figure(data=[edge_trace, node_trace],
+                        layout=go.Layout(
+                            showlegend=False,
+                            hovermode='closest',
+                            margin=dict(b=0, l=0, r=0, t=0),
+                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )
+
+        st.plotly_chart(fig, width="stretch")
